dbc247176ee7e40293244a9dcb9c6a51e1ca9736
Marina Kononovich <marina.kononovich.97@mail.ru>
2018-11-01 23:00:20 +0600

    Ready Probabilistic latent semantic analysis

--- library/examples/LW3/LW3.py
+++ library/examples/LW3/LW3.py
@@ -1,1 +1,9 @@
+from library.algoritms.plsa.PLSA import PLSA
+from library.algoritms.plsa.Corpus import Corpus
 
+corpus = Corpus()
+corpus.generate_corpus("C:\\Users\\miga\\Desktop\\data-lws\\library\\data\\lenta_ru.csv")
+
+plsa = PLSA(corpus)
+plsa.init_stop_words()
+plsa.fit_em_algo()
